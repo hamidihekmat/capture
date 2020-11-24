5f3cf22a73dbc3e19c5b42b5ae9101451fc4f316
Hekmat Hamidi <theonehamidi@gmail.com>
2020-11-23 21:04:45 -0500

    added services section

--- src/pages/AboutUs.js
+++ src/pages/AboutUs.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import About from '../components/About';
+import Services from '../components/Services';
 
 function AboutUs() {
   return (
     <>
       <About />
+      <Services />
     </>
   );
 }
